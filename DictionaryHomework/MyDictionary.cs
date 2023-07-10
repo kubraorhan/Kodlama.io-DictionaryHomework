4292cdec683cff8016f1226aa9ec57dd02265e01
Kübra Orhan <kubraorhanb@gmail.com>
2023-07-10 09:47:19 +0300

    Add project files.

---
+++ DictionaryHomework/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryHomework
+{
+    public class MyDictionary<Tkey,TValue>
+    {
+        Tkey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+           keys = new Tkey[0];
+           values = new TValue[0];
+        }
+        public void Add(Tkey key, TValue value)
+        {
+            var TempKeyArray = keys;
+            keys = new Tkey[keys.Length + 1];
+
+            var TempValueArray = values;
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < TempKeyArray.Length; i++)
+            {
+                keys[i] = TempKeyArray[i];
+                values[i] = TempValueArray[i];
+            }
+
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            Console.WriteLine(key + ".value is " +value);
+            
+        }
+
+        
+    }
+}
---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictionaryHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            MyDictionary <int, string> idAndNames = new MyDictionary<int,string> ();
+
+            idAndNames.Add(1, "Kübra");
+            idAndNames.Add(2, "Merve");
+            idAndNames.Add(3, "Necati");
+            idAndNames.Add(4, "Can");
+
+        }
+    }
+}
